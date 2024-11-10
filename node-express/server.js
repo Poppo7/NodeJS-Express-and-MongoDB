5325d94e5c9e4c54bdcd31a5bff22130a95c220c
JustinCodes0 <jwhite1152@hotmail.com>
2024-11-10 00:17:54 -0800

    feat: Intro to Express Part 2

--- node-express/server.js
+++ node-express/server.js
@@ -1,19 +1,27 @@
-
-
 const express = require('express');
+const morgan = require('morgan');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
 
+// Use Morgan middleware for logging
+app.use(morgan('dev'));
+
+// Serve static files from the "public" directory
+app.use(express.static(__dirname + '/public'));
+
+// Default route if no static file matches
 app.use((req, res) => {
-    console.log(req.headers);
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     res.end('<html><body><h1>This is an Express Server</h1></body></html>');
 });
 
+// Start the server
 app.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+//type "node server.js" to start the server in the terminal.
