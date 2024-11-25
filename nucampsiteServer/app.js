0dde26e90ca9c839dfac993b867ee7b7b7e6997e
JustinCodes0 <jwhite1152@hotmail.com>
2024-11-24 21:05:04 -0800

    chore: Express Session Part 2

--- nucampsiteServer/app.js
+++ nucampsiteServer/app.js
@@ -1,20 +1,21 @@
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
+const createError = require('http-errors');
+const mongoose = require('mongoose');
+
 const campsiteRouter = require('./routes/campsiteRouter');
 const promotionRouter = require('./routes/promotionRouter');
 const partnerRouter = require('./routes/partnerRouter');
-const session = require('express-session');
-const FileStore = require('session-file-store')(session);
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const app = express();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-const mongoose = require('mongoose');
-
+// MongoDB connection
 const url = 'mongodb://localhost:27017/nucampsite';
 const connect = mongoose.connect(url, {});
 
@@ -22,9 +23,7 @@ connect.then(() => console.log('Connected correctly to server'),
   err => console.log(err)
 );
 
-var app = express();
-
-// view engine setup
+// Middleware setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
@@ -34,42 +33,17 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-app.use('/campsites', campsiteRouter);
-app.use('/promotions', promotionRouter);
-app.use('/partners', partnerRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-//app.use(cookieParser('12345-67890-09876-54321'));
-
+// Session middleware
 app.use(session({
     name: 'session-id',
-    secret: '12345-67890-09876-54321',
+    secret: '12345-67890-09876-54321', // Use environment variables in production
     saveUninitialized: false,
     resave: false,
     store: new FileStore()
 }));
 
+// Authentication middleware
 function auth(req, res, next) {
-    console.log(req.session);
-
     if (!req.session.user) {
         const authHeader = req.headers.authorization;
         if (!authHeader) {
@@ -82,7 +56,7 @@ function auth(req, res, next) {
         const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
         const user = auth[0];
         const pass = auth[1];
-        if (user === 'admin' && pass === 'password') {
+        if (user === 'admin' && pass === 'password') { // Replace with secure credential storage
             req.session.user = 'admin';
             return next(); // authorized
         } else {
@@ -102,6 +76,27 @@ function auth(req, res, next) {
     }
 }
 
+// Routes
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// Apply `auth` middleware to protected routes
 app.use(auth);
+app.use('/campsites', campsiteRouter);
+app.use('/promotions', promotionRouter);
+app.use('/partners', partnerRouter);
+
+// Error handling
+app.use(function (req, res, next) {
+    next(createError(404));
+});
+
+app.use(function (err, req, res, next) {
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    res.status(err.status || 500);
+    res.render('error');
+});
 
 module.exports = app;
