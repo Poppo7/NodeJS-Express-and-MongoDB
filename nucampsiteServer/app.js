d8735e348d331992fe2f78315ee49dbd8c395e9f
JustinCodes0 <jwhite1152@hotmail.com>
2024-11-26 23:51:33 -0800

    chore: passport

--- nucampsiteServer/app.js
+++ nucampsiteServer/app.js
@@ -6,6 +6,8 @@ const session = require('express-session');
 const FileStore = require('session-file-store')(session);
 const createError = require('http-errors');
 const mongoose = require('mongoose');
+const passport = require('passport');
+const authenticate = require('./authenticate');
 
 const campsiteRouter = require('./routes/campsiteRouter');
 const promotionRouter = require('./routes/promotionRouter');
@@ -19,8 +21,9 @@ const app = express();
 const url = 'mongodb://localhost:27017/nucampsite';
 const connect = mongoose.connect(url, {});
 
-connect.then(() => console.log('Connected correctly to server'),
-  err => console.log(err)
+connect.then(
+  () => console.log('Connected correctly to server'),
+  (err) => console.log(err)
 );
 
 // Middleware setup
@@ -33,47 +36,31 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Session middleware
-app.use(session({
+// Passport and session configuration
+app.use(
+  session({
     name: 'session-id',
-    secret: '12345-67890-09876-54321', // Use environment variables in production
+    secret: '12345-67890-09876-54321', // Replace with environment variable for production
     saveUninitialized: false,
     resave: false,
-    store: new FileStore()
-}));
+    store: new FileStore(),
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Authentication middleware
 function auth(req, res, next) {
-    if (!req.session.user) {
-        const authHeader = req.headers.authorization;
-        if (!authHeader) {
-            const err = new Error('You are not authenticated!');
-            res.setHeader('WWW-Authenticate', 'Basic');
-            err.status = 401;
-            return next(err);
-        }
+  console.log(req.user);
 
-        const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-        const user = auth[0];
-        const pass = auth[1];
-        if (user === 'admin' && pass === 'password') { // Replace with secure credential storage
-            req.session.user = 'admin';
-            return next(); // authorized
-        } else {
-            const err = new Error('You are not authenticated!');
-            res.setHeader('WWW-Authenticate', 'Basic');
-            err.status = 401;
-            return next(err);
-        }
-    } else {
-        if (req.session.user === 'admin') {
-            return next();
-        } else {
-            const err = new Error('You are not authenticated!');
-            err.status = 401;
-            return next(err);
-        }
-    }
+  if (!req.user) {
+    const err = new Error('You are not authenticated!');
+    err.status = 401;
+    return next(err);
+  } else {
+    return next();
+  }
 }
 
 // Routes
@@ -88,15 +75,15 @@ app.use('/partners', partnerRouter);
 
 // Error handling
 app.use(function (req, res, next) {
-    next(createError(404));
+  next(createError(404));
 });
 
 app.use(function (err, req, res, next) {
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-    res.status(err.status || 500);
-    res.render('error');
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 module.exports = app;
